ca69f6c7f4203633a03f1adc59b70bab56a2e4b6
kazsoft <kazsoft@pc931u>
2016-09-08 16:58:42 -0500

    Expanded output text.

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -3,7 +3,8 @@ public class HelloWorldMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Hello Dan!");
+		System.out.println("This code is maintaned in git!");
 
 	}
 
