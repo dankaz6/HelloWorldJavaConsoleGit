8dd5472f8c249929f2d6cd5e507377fa516ae8b1
dan <dan@pc007u>
2016-09-08 20:09:02 -0500

    Added a sample class for simple math operations

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -2,10 +2,17 @@
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello World! Hello Dan!");
 		System.out.println("This code is maintaned in git!");
 
+		SimpleMath sm = new SimpleMath();
+		sm.setX(5);
+		sm.setY(3);
+
+		System.out.println("Sum=" + sm.Sum());
+		System.out.println("Difference=" + sm.Difference());
+		System.out.println("Product=" + sm.Product());
 	}
 
 }
---
+++ src/SimpleMath.java
@@ -0,0 +1,37 @@
+
+public class SimpleMath {
+
+	private int x = 0;
+	private int y = 0;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int Sum() {
+		return (x + y);
+
+	}
+
+	public int Difference() {
+		return (x-y);
+	}
+	
+	public int Product () {
+		return (x*y);
+		
+	}
+	
+}
